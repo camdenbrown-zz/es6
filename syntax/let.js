a36b45e159c8680fbda62949aa026c98b1010f35
Camden Brown <camden.brown@metova.com>
2017-06-06 15:02:41 -0500

    scope with let and callbacks

--- syntax/let.js
+++ syntax/let.js
@@ -19,3 +19,12 @@ console.log(productId); // 12
   let productId = 1221;
 }
 console.log(productId); //undefined
+
+//The code gets evaluated, so when you call the function productId is declared
+// and is set to 12
+function updateProductId() {
+  productId = 12;
+}
+let productId = null;
+updateProductId();
+console.log(productId);
